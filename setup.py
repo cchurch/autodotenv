c7bb8e09c85431371d609237660d60bfb97cc7e8
Chris Church <chris@ninemoreminutes.com>
2020-10-17 00:21:41 -0400

    Switch to using setuptools-twine, add/check Python 3.9 support.

--- setup.py
+++ setup.py
@@ -9,42 +9,6 @@ from setuptools import Command, setup
 import setuptools.command.build_py as orig_build_py
 
 
-class BaseTwineCommand(Command):
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        self.run_command('sdist')
-        self.get_finalized_command('sdist')
-        self.run_command('bdist_wheel')
-        self.get_finalized_command('bdist_wheel')
-        dist_files = [df[2] for df in self.distribution.dist_files]
-        self.spawn(['twine', self.twine_subcommand] + dist_files)
-
-    sub_commands = [
-        ('sdist', lambda self: True),
-        ('bdist_wheel', lambda self: True),
-    ]
-
-
-class TwineCheckCommand(BaseTwineCommand):
-
-    description = 'Check distribution files with twine'
-    twine_subcommand = 'check'
-
-
-class TwineUploadCommand(BaseTwineCommand):
-
-    description = 'Upload distribution files with twine'
-    twine_subcommand = 'upload'
-
-
 class UnsupportedCommand(Command):
 
     description = 'This command is not supported'
@@ -76,8 +40,6 @@ setup(
     },
     cmdclass={
         'build_py': BuildPyCommand,
-        'twine_check': TwineCheckCommand,
-        'twine_upload': TwineUploadCommand,
         'unsupported': UnsupportedCommand,
     },
 )
